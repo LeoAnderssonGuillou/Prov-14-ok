ee037c5ae3e3bd796f99f9adfd80d6a8aaafba93
Leo Andersson Guillou <leo.anderssonguillo@learnet.se>
2020-10-14 13:59:11 +0200

    Hello world

---
+++ Prov 14 okt Folder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prov_14_okt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
